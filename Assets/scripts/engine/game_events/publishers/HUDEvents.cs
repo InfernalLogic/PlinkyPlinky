cced8571f5f5e5272af10d5c7e2a0ee8e987f960
Jakob Daniels <daniels.jakob@gmail.com>
2014-12-08 22:46:03 -0800

    text scaling
    still need to fix the stat displayer for plinkagon points and the score
    ticker

---
+++ Assets/scripts/engine/game_events/publishers/HUDEvents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class HUDEvents
+{
+  private static Publisher<RescaleHUDEvent> publisher = new Publisher<RescaleHUDEvent>();
+
+  public static void AddSubscriber(Subscriber<RescaleHUDEvent> subscriber)
+  {
+    publisher.AddSubscriber(subscriber);
+  }
+
+  public static void Publish(RescaleHUDEvent message)
+  {
+    publisher.PublishMessage(message);
+  }
+}
