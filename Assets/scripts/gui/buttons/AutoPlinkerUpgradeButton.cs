4d350e6498c1cf8ef384c926df93d970205eb41e
Jakob Daniels <daniels.jakob@gmail.com>
2015-02-24 14:41:52 -0800

    v1.01 released

--- Assets/scripts/gui/buttons/AutoPlinkerUpgradeButton.cs
+++ Assets/scripts/gui/buttons/AutoPlinkerUpgradeButton.cs
@@ -13,9 +13,18 @@ public class AutoPlinkerUpgradeButton : UpgradeButton
 
   protected override void DisplayTextLabel()
   {
+    string cooldown;
+
+    if (auto_plinker.Cooldown > 10.0f)
+      cooldown = "N/A";
+    else
+    {
+      cooldown = auto_plinker.Cooldown.ToString("n2");
+      cooldown += "s";
+    }
+
     GUI.Label(label_display_rect.GetRect(),
               button_text + "\nCost: " + target_upgrade.GetUpgradeCost() + "\nCooldown: " +
-              (auto_plinker.Cooldown.ToString("n2")) + "s",
-              label_style);
+              cooldown, label_style);
   }
 }
